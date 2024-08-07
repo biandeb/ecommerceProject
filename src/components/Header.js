843b1ea5499171b6b14853c75ad29a62ec41fc08
Bianca Deblinger <biandeb96@gmail.com>
2024-08-07 12:45:15 -0300

    creo los archivos iniciales correspondientes

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  )
+}
+export default Header
+const styles = StyleSheet.create({})
---
+++ src/screens/ItemListCategories.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const ItemListCategories = () => {
+  return (
+    <View>
+      <Text>ItemListCategories</Text>
+    </View>
+  )
+}
+export default ItemListCategories
+const styles = StyleSheet.create({})
